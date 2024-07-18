69ede41e76209d554337cce4930f66d9c741dada
KranzA <107824196+KranzA@users.noreply.github.com>
2024-07-18 13:19:33 -0600

    Update enter_hours.php

--- enter_hours.php
+++ enter_hours.php
@@ -56,6 +56,19 @@
 			}
 			echo "</td><td><input type='checkbox' id='pvacation' name='$day.pvacation' value='Paid'></td></tr>";
 		}
+		for ($x = 1; $x < 7-date("w"); $x++) {
+			$time = time() + $x * $mins;
+			$day = $name[date("w",$time)];
+			$date = $day." ".date("m/d",$time);
+			echo "<tr><td class='$day'>$date</td>";
+			echo "<input type='hidden' name='$day.date' value='$date' class='$day'/>";
+			echo "<td><select name='$day.hours' id='numhours'>";
+			echo "<option value='0'></option>";
+			for ($i=1;$i<=24;$i++) {
+				echo "<option value='$i'>$i</option>";
+			}
+			echo "</td><td><input type='checkbox' id='pvacation' name='$day.pvacation' value='Paid'></td></tr>";
+		}
 		
 		
 	?>
