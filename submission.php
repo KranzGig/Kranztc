6deb0dcd6d8ece4ca788684c5fc7f56eb5c3d704
KranzA <107824196+KranzA@users.noreply.github.com>
2024-07-18 11:21:42 -0600

    Update submission.php

--- submission.php
+++ submission.php
@@ -17,12 +17,12 @@
 		$date = $_POST[$day . 'date'];
 		$id = $_SESSION['id'];
 		$sql = "SELECT * FROM Hours WHERE Date= . $date . AND EmpID= . $id";
-		$result = $conn->query($sql);
-		if ($result->num_rows > 0) {
-			echo "True";
-		} else {
-			echo "False";
-		}
+		//$result = $conn->query($sql);
+		//if ($result->num_rows > 0) {
+		//	echo "True";
+		//} else {
+		//	echo "False";
+		//}
 	}
 	echo $_POST[$days[0] . "date"];
 ?>
