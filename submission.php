dab632a95ef71d3733c95bfbed337254bf500bb5
KranzA <107824196+KranzA@users.noreply.github.com>
2024-07-18 10:40:02 -0600

    Update submission.php

--- submission.php
+++ submission.php
@@ -2,5 +2,17 @@
 	//header('Location: index.html');
 	//exit;
 	session_start();
+	$servername = "127.0.0.1:3306";
+	$username = "u751975974_kranz";
+	$password = "Dradbgon12";
+	$dbname = "u751975974_TestDB";
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+
+	// Check connection
+	if ($conn->connect_error) {
+ 		die("Connection failed: " . $conn->connect_error);
+	}
 	echo $_SESSION['id'];
 ?>
