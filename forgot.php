7e2ebf54632a50625ea4463e7c406cb39b763117
KranzA <107824196+KranzA@users.noreply.github.com>
2024-07-19 14:40:11 -0600

    Update and rename forgot.html to forgot.php

--- forgot.php
+++ forgot.php
@@ -16,4 +16,24 @@
       </form>
   </p>
 </body>
+<?php
+  $servername = "127.0.0.1:3306";
+  $username = "u751975974_kranz";
+  $password = "Dradbgon12";
+  $dbname = "u751975974_TestDB";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  
+  if ($stmt = $conn->prepare('SELECT id FROM accounts WHERE email = ?')) {
+    $stmt->bind_param('s', $_POST['email']);
+    $stmt->execute();
+	  $stmt->store_result();
+    if ($stmt->num_rows > 0) {
+
+    } else {
+      echo 'Incorrect email!';
+    }
+  }
+?>
 </html>
