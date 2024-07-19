3e9fb336c2a2d5dc8b070bcadf8f16d24cbb05fc
KranzA <107824196+KranzA@users.noreply.github.com>
2024-07-19 14:42:19 -0600

    Update forgot.php

--- forgot.php
+++ forgot.php
@@ -10,30 +10,10 @@
 </head>
 <body>
   <p>
-      <form action="" method="post">
+      <form action="email.php" method="post">
           <input type="text" id="email" name="email" placeholder="email">
           <input type="submit" value="Enter">
       </form>
   </p>
 </body>
-<?php
-  $servername = "127.0.0.1:3306";
-  $username = "u751975974_kranz";
-  $password = "Dradbgon12";
-  $dbname = "u751975974_TestDB";
-
-  // Create connection
-  $conn = new mysqli($servername, $username, $password, $dbname);
-  
-  if ($stmt = $conn->prepare('SELECT id FROM accounts WHERE email = ?')) {
-    $stmt->bind_param('s', $_POST['email']);
-    $stmt->execute();
-	  $stmt->store_result();
-    if ($stmt->num_rows > 0) {
-
-    } else {
-      echo 'Incorrect email!';
-    }
-  }
-?>
 </html>
