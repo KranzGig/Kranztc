8f102d8ea4aa426c3d60b8b8ff12265d68fb4322
KranzA <107824196+KranzA@users.noreply.github.com>
2024-07-19 15:08:13 -0600

    Update email.php

--- email.php
+++ email.php
@@ -1,7 +1,4 @@
 <?php
-  //require 'vendor/autoload.php';
-  //use PHPMailer\PHPMailer\PHPMailer;
-
   $servername = "127.0.0.1:3306";
   $username = "u751975974_kranz";
   $password = "Dradbgon12";
@@ -17,30 +14,15 @@
     if ($stmt->num_rows > 0) {
 	$stmt->bind_result($id);
 	$stmt->fetch();
+	echo $id;
 	$code = uniqid();
 	if ($stmt = $conn->prepare("UPDATE accounts SET code=? WHERE id=$id")) {
 		$stmt->bind_param('s', password_hash($code,PASSWORD_DEFAULT));
 		$stmt->execute();
-		echo 'Check your email for the reset link';
-	        /*$mail = new PHPMailer;
-		   $mail->isSMTP();
-		   $mail->SMTPDebug = 2;
-		   $mail->Host = 'smtp.hostinger.com';
-		   $mail->Port = 587;
-		   $mail->SMTPAuth = true;
-		   $mail->Username = 'automail@kranztc.com';
-		   $mail->Password = 'DragonSandd0gs-';
-		   $mail->setFrom('automail@kranztc.com', 'Kranz Tech Consulting');
-		   $mail->addReplyTo('automail@kranztc.com', 'Kranz Tech Consulting');
-		   $mail->addAddress('kranz.amber1@gmail.com', 'Amber Kranz');
-		   $mail->Subject = 'Time Tracker Password Reset';
-		   $mail->Body = "Use the following link to reset your password: kranztc.com/reset.php?id=".password_hash($code,PASSWORD_DEFAULT);
-		   $mail->addAttachment('result.csv');
-		   if (!$mail->send()) {
-		       echo 'Mailer Error: ' . $mail->ErrorInfo;
-		   } */
-	} else {
-		      echo 'Incorrect email!';
-		    }
+	}
+	echo $code;
+    } else {
+      echo 'Incorrect email!';
+    }
   }
 ?>
