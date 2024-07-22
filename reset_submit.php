76c4816751edf979e024faadb436855537d3a984
KranzA <107824196+KranzA@users.noreply.github.com>
2024-07-22 12:13:18 -0600

    Update reset_submit.php

--- reset_submit.php
+++ reset_submit.php
@@ -14,8 +14,8 @@ if ($_POST['pword'] != $_POST['repword']) {
 	exit('Passwords do not match');
 }
 echo "one";
-/*if ($stmt = $conn->prepare('SELECT id FROM accounts WHERE code = ?')) {
-	echo password_hash($_POST['URLid'],PASSWORD_DEFAULT)
+if ($stmt = $conn->prepare('SELECT id FROM accounts WHERE code = ?')) {
+	echo password_hash($_POST['URLid'],PASSWORD_DEFAULT);
 	$stmt->bind_param('s', password_hash($_POST['URLid'],PASSWORD_DEFAULT));
 	$stmt->execute();
 	$stmt->store_result();
@@ -35,6 +35,6 @@ echo "one";
     		    header('Location: enter_hours.php');
     	    }
 	}
-}*/
+}
 //echo $_POST['URLid'];
 ?>
