c23fc32fe11373478c4f33fafdd4fa43762b4e93
KranzA <107824196+KranzA@users.noreply.github.com>
2024-07-22 09:55:21 -0600

    Update reset.php

--- reset.php
+++ reset.php
@@ -22,7 +22,7 @@ $url_components = parse_url($url);
 // Use parse_str() function to parse the
 // string passed via URL
 parse_str($url_components['query'], $params);
-echo "value";
+echo "value=".$params['id'];
 //echo $params['id'];
  
 ?>  
