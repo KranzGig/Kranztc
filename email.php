8042e8959c17230bea7c556264d2633a6b1836eb
KranzA <107824196+KranzA@users.noreply.github.com>
2024-07-22 12:20:59 -0600

    Update email.php

--- email.php
+++ email.php
@@ -1,4 +1,4 @@
-<?php
+s<?php
   $servername = "127.0.0.1:3306";
   $username = "u751975974_kranz";
   $password = "Dradbgon12";
@@ -16,7 +16,7 @@
 	$stmt->fetch();
 	$code = uniqid();
 	if ($stmt = $conn->prepare("UPDATE accounts SET code=? WHERE id=$id")) {
-		$stmt->bind_param('s', password_hash($code,PASSWORD_DEFAULT));
+		$stmt->bind_param('s', md5($code));
 		$stmt->execute();
 	}
 	echo "kranztc.com/reset.php?id=".$code;
