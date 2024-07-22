ee3c29cd761fa60db5059252473b940d1d5dfc47
KranzA <107824196+KranzA@users.noreply.github.com>
2024-07-22 09:52:24 -0600

    Update reset.php

--- reset.php
+++ reset.php
@@ -26,7 +26,7 @@ $url_components = parse_url($url);
 // Use parse_str() function to parse the
 // string passed via URL
 parse_str($url_components['query'], $params);
-echo "<input type='hidden' name='URLid' value=$params['id']/>";
+echo "<input type='hidden' name='URLid' value=$params['id'] class='URLid'/>";
 //echo $params['id'];
  
 ?>
