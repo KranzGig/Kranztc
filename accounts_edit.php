7ecc65b853555d6735275a7400ad77a4cc9ec833
KranzA <107824196+KranzA@users.noreply.github.com>
2024-07-24 16:24:36 -0600

    Update accounts_edit.php

--- accounts_edit.php
+++ accounts_edit.php
@@ -36,7 +36,7 @@
     		while($row = $result->fetch_assoc()) {
 			echo "<tr>";
 			echo "<th>Email:</th>";
-			echo "<input type='text' value='".$row['email']."' id='".$row['email']."'>";
+			echo "<td><input type='text' value='".$row['email']."' id='".$row['email']."'></td>";
 			//echo $row['email'];
 			echo "</td></tr>";
 			echo "<tr>";
