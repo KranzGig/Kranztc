6b76cfffe3986a6d68794659fe34671a2a9d3146
KranzA <107824196+KranzA@users.noreply.github.com>
2024-07-24 16:54:36 -0600

    Update edit_submit.php

--- edit_submit.php
+++ edit_submit.php
@@ -17,7 +17,8 @@
 	 if ($result->num_rows > 0) {
     		while($row = $result->fetch_assoc()) {
           $email_orig = $row['email'];
-	echo $_POST["'email-".$email_orig."'"];
+	echo "email-".$email_orig;
+	echo $_POST["email-".$email_orig];
           $email = $_POST["email-".$email_orig];
           $admin = 1;
           if (isset($_POST["admin-".$email])) {
