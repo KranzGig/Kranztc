a623573cd2a20a4f1a224e069884e8a0a9f44af2
KranzA <107824196+KranzA@users.noreply.github.com>
2024-07-24 15:01:47 -0600

    Create add_submit.php

---
+++ add_submit.php
@@ -0,0 +1,22 @@
+<?php
+  $servername = "127.0.0.1:3306";
+  $username = "u751975974_kranz";
+  $password = "Dradbgon12";
+  $dbname = "u751975974_TestDB";
+  
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  $admin = 1;
+  if (isset($_POST['caretaker'])) {
+    $admin = 0;
+  }
+  $sql = "INSERT INTO accounts (email, admin)
+  VALUES ('". $_POST['email'] . "','" . $admin ."')";
+  $conn->query($sql);
+  header('Location: accounts.php');
+?>
