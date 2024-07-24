ab2f79c172997ca77fdfbe0a88e40fb6181e0ff5
KranzA <107824196+KranzA@users.noreply.github.com>
2024-07-24 15:33:22 -0600

    Create delete_submit.php

---
+++ delete_submit.php
@@ -0,0 +1,18 @@
+<?php
+  $servername = "127.0.0.1:3306";
+  $username = "u751975974_kranz";
+  $password = "Dradbgon12";
+  $dbname = "u751975974_TestDB";
+  
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  $sql = "DELETE FROM accounts WHERE email=".$_POST['email'];
+  $conn->query($sql);
+  header('Location: accounts.php');
+?>
