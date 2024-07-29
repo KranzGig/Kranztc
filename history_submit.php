e79bbe0e56e18703559de6f386e527afc1ffa851
KranzA <107824196+KranzA@users.noreply.github.com>
2024-07-29 14:54:32 -0600

    Update history_submit.php

--- history_submit.php
+++ history_submit.php
@@ -30,11 +30,11 @@
 		if ($result->num_rows > 0) {
 			$sql = "UPDATE Hours SET Hours=$hours, Vacation=$vacation WHERE Date=$date AND EmpID=$id";
 			echo $sql;
-			$conn->query($sql);
+			//$conn->query($sql);
 		} else {
 			$sql = "INSERT INTO Hours VALUES ($date, $hours, $vacation, $id)";
-			//echo $sql;
-			$conn->query($sql);
+			echo $sql;
+			//$conn->query($sql);
 		}
 	}
 	//header('Location: history.php');
