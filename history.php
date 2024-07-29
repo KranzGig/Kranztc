8740d53568cac3e32f3a6fc1eaa24fac584242c3
KranzA <107824196+KranzA@users.noreply.github.com>
2024-07-29 13:41:45 -0600

    Update history.php

--- history.php
+++ history.php
@@ -91,7 +91,7 @@
 				$num = $row["Hours"];
 				$vacation = $row["Vacation"];
 				echo "<tr><td>$date</td>";
-				echo "<td><input type='text' value=$num class='hidden' hidden>$num</td>";
+				echo "<td hidden><input type='text' value=$num class='hidden' hidden>$num</td>";
 				if ($vacation) {
 					echo "<td><input type='checkbox' class='hidden' name='vacation' checked hidden>Yes</td></tr>";
 				} else {
