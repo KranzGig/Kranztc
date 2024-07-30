27f44c5fcfa998358cbb30008906ee96c786b44d
KranzA <107824196+KranzA@users.noreply.github.com>
2024-07-30 13:46:07 -0600

    Update submission.php

--- submission.php
+++ submission.php
@@ -32,11 +32,11 @@
 		}
 		if ($result->num_rows > 0) {
 			$sql = "UPDATE Hours SET Hours=$hours, Vacation=$vacation WHERE Date=$date AND EmpID=$id";
-			echo $sql;
+			//echo $sql;
 			$conn->query($sql);
 		} else {
 			$sql = "INSERT INTO Hours VALUES ($date, $hours, $vacation, $id)";
-			echo $sql;
+			//echo $sql;
 			$conn->query($sql);
 		}
 	}
