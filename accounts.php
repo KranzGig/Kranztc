99ed8c9549df866a81ba7421c83dac8badaf26b7
KranzGig <automail@kranztc.com>
2024-08-01 16:54:21 -0600

    Update accounts.php

--- accounts.php
+++ accounts.php
@@ -112,13 +112,13 @@
   <button class="square_current" onclick="window.location.href='accounts_edit.php'">
     /
   </button>
-  <a href="#" onclick="window.location.href='accounts_edit.php'">Edit Profile</a><br>
+  Edit Profile<br>
   </div>
   <div class="button1">
   <button class="square" onclick="removeHidden()">
     +
   </button>
-  <a href="#" onclick="removeHidden()">Add Profile</a>
+  Add Profile
   </div><br /><br />
 
 <script>
