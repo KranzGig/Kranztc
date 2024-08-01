060bd5d4eb65563696b444fa0ef272a9077ab4a8
nightflame137 <95541729+nightflame137@users.noreply.github.com>
2024-08-01 14:47:07 -0600

    Update forgot.php

--- forgot.php
+++ forgot.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <title>Time Tracker Login</title>
-  <link rel="stylesheet" type="text/css" href="style.css"?v=1.1>
+  <link rel="stylesheet" type="text/css" href="style.css"?v=1.2>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
