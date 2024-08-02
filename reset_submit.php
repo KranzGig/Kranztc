001191abc427cdd612dae56df71fe1b8178ae73f
KranzA <107824196+KranzA@users.noreply.github.com>
2024-08-02 12:50:05 -0600

    Update reset_submit.php

--- reset_submit.php
+++ reset_submit.php
@@ -8,20 +8,11 @@ $dbname = "u751975974_TestDB";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 //echo ($_POST['pword'] == '');
-$url = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-     
-// Use parse_url() function to parse the URL 
-// and return an associative array which
-// contains its various components
-$url_components = parse_url($url);
- 
-// Use parse_str() function to parse the
-// string passed via URL
-parse_str($url_components['query'], $params);
+
 if ($_POST['pword'] == '' || $_POST['repword'] == '' ) {
 	// Could not get the data that should have been sent.
 	//echo "hi1";
-        header('Location: $url&error=1');
+        //header('Location: $url&error=1');
 } 
 if ($_POST['pword'] != $_POST['repword']) {
 	exit('Passwords do not match');
