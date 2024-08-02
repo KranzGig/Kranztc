746a9ff05010ccc63d48fa0d5d805bb20b55d871
KranzA <107824196+KranzA@users.noreply.github.com>
2024-08-02 14:48:47 -0600

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -13,9 +13,10 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 if ( !isset($_POST['uname'], $_POST['pword']) ) {
+	echo "one";
 	// Could not get the data that should have been sent.
-	header('Location: index.html?error=1');
-        exit;
+	//header('Location: index.html?error=1');
+        //exit;
 }
 if ($stmt = $conn->prepare('SELECT id, password, admin, name FROM accounts WHERE email = ?')) {
 	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
@@ -38,10 +39,11 @@ if ($stmt = $conn->prepare('SELECT id, password, admin, name FROM accounts WHERE
     		    $_SESSION['uname'] = $_POST['uname'];
     		    $_SESSION['id'] = $id;
 		    $_SESSION['name'] = $name;
+		    echo "two";
 		    if ($admin == 0) {
-    		    	header('Location: enter_hours.php');
+    		    	//header('Location: enter_hours.php');
 		    } else {
-			header('Location: accounts.php');
+			//header('Location: accounts.php');
 		    }
     	    }
     	    
@@ -55,20 +57,23 @@ if ($stmt = $conn->prepare('SELECT id, password, admin, name FROM accounts WHERE
     		$_SESSION['uname'] = $_POST['uname'];
     		$_SESSION['id'] = $id;
 		$_SESSION['name'] = $name;
-    		if ($admin == 0) {
-    		    	header('Location: enter_hours.php');
+    		echo "three";
+		if ($admin == 0) {
+    		    	//header('Location: enter_hours.php');
 		    } else {
-			header('Location: accounts.php');
+			//header('Location: accounts.php');
 		    }
     	} else {
-    		// Incorrect password
-    		header('Location: index.html?error=1');
-		exit;
+    		echo "four";
+		// Incorrect password
+    		//header('Location: index.html?error=1');
+		//exit;
     	}
     } else {
-	// Incorrect username
-	header('Location: index.html?error=1');
-        exit;
+	echo "five";
+	    // Incorrect username
+	//header('Location: index.html?error=1');
+        //exit;
     }
 }
 ?>
