3de24dd9311f44d1f1db0e0928cb9a5b88289cf9
KranzA <107824196+KranzA@users.noreply.github.com>
2024-08-02 10:40:28 -0600

    Update reset_submit.php

--- reset_submit.php
+++ reset_submit.php
@@ -8,15 +8,18 @@ $dbname = "u751975974_TestDB";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ( !isset($_POST['pword'], $_POST['repword']) ) {
 	// Could not get the data that should have been sent.
-	header('Location:reset.php?error=1');
-	exit;
+	echo "one";
+	//header('Location:reset.php?error=1');
+	//exit;
 	
 }
 if ($_POST['pword'] != $_POST['repword']) {
-	header('Location:reset.php?error=1');
-	exit;
+	//header('Location:reset.php?error=1');
+	//exit;
+	echo "two";
 }
 if ($stmt = $conn->prepare('SELECT id FROM accounts WHERE code = ?')) {
+	echo "three";
 	$stmt->bind_param('s', md5($_POST['URLid']));
 	$stmt->execute();
 	$stmt->store_result();
@@ -30,7 +33,7 @@ if ($stmt = $conn->prepare('SELECT id FROM accounts WHERE code = ?')) {
     		    $_SESSION['loggedin'] = TRUE;
     		    $_SESSION['uname'] = $_POST['uname'];
     		    $_SESSION['id'] = $id;
-    		    header('Location: enter_hours.php');
+    		    //header('Location: enter_hours.php');
     	    }
 	}
 }
