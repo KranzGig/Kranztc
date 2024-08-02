54d3f2aaf293c00281ad207145b3e57a2e5f9e8a
KranzA <107824196+KranzA@users.noreply.github.com>
2024-08-02 11:03:09 -0600

    Update reset.php

--- reset.php
+++ reset.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="utf-8">
     <title>Time Tracker Login</title>
-  <link rel="stylesheet" type="text/css" href="style3.css?v=1.6">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="style.css?v=2.3">
 </head>
-<body>
+<body onload="checkIncorrect()">
 <form action="reset_submit.php" method="post">
     <?php
 // Initialize URL to the variable
@@ -25,13 +25,27 @@ parse_str($url_components['query'], $params);
 echo "<input type='hidden' name='URLid' value='".$params['id']."'/>";
 //echo $params['id'];
  
-?>  
+?> 
+<div class='reset'>
     New Password:<br />
       <input type="password" id="pword" name="pword" placeholder="Password"><br /><br />
-      Re Enter Password:<br />
+      Reenter Password:<br />
       <input type="password" id="repword" name="repword" placeholder="Password"><br /><br />
+      <p id='incorrect' style="display:none;">Please input matching passwords</p>
       <input type="submit" value="Enter">
+</div>
 </form>
 
+<script type="text/javascript">
+	function checkIncorrect() {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        if (urlParams.get("error") == "1") {
+            document.getElementById("incorrect").style.removeProperty('display');
+        }
+    }
+    
+</script>
+    
 </body>
 </html>
