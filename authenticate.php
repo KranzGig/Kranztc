ef6a01711d07cf22b48d923fd8594bcea7c7ace8
KranzA <107824196+KranzA@users.noreply.github.com>
2024-08-02 14:51:47 -0600

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -13,10 +13,10 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 if ( !isset($_POST['uname'], $_POST['pword']) ) {
-	echo "one";
+	//echo "one";
 	// Could not get the data that should have been sent.
-	//header('Location: index.html?error=1');
-        //exit;
+	header('Location: index.html?error=1');
+        exit;
 }
 if ($stmt = $conn->prepare('SELECT id, password, admin, name FROM accounts WHERE email = ?')) {
 	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
@@ -39,11 +39,13 @@ if ($stmt = $conn->prepare('SELECT id, password, admin, name FROM accounts WHERE
     		    $_SESSION['uname'] = $_POST['uname'];
     		    $_SESSION['id'] = $id;
 		    $_SESSION['name'] = $name;
-		    echo "two";
+		    //echo "two";
 		    if ($admin == 0) {
-    		    	//header('Location: enter_hours.php');
+    		    	header('Location: enter_hours.php');
+			    exit;
 		    } else {
-			//header('Location: accounts.php');
+			header('Location: accounts.php');
+			    exit;
 		    }
     	    }
     	    
@@ -57,23 +59,25 @@ if ($stmt = $conn->prepare('SELECT id, password, admin, name FROM accounts WHERE
     		$_SESSION['uname'] = $_POST['uname'];
     		$_SESSION['id'] = $id;
 		$_SESSION['name'] = $name;
-    		echo "three";
+    		//echo "three";
 		if ($admin == 0) {
-    		    	//header('Location: enter_hours.php');
+    		    	header('Location: enter_hours.php');
+			exit;
 		    } else {
-			//header('Location: accounts.php');
+			header('Location: accounts.php');
+			exit;
 		    }
     	} else {
-    		echo "four";
+    		//echo "four";
 		// Incorrect password
-    		//header('Location: index.html?error=1');
-		//exit;
+    		header('Location: index.html?error=1');
+		exit;
     	}
     } else {
-	echo "five";
+	//echo "five";
 	    // Incorrect username
-	//header('Location: index.html?error=1');
-        //exit;
+	header('Location: index.html?error=1');
+        exit;
     }
 }
 ?>
