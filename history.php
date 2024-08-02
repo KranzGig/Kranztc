11220840138424c615e38a7fa0c62bc34916cabf
KranzA <107824196+KranzA@users.noreply.github.com>
2024-08-02 10:31:10 -0600

    Update history.php

--- history.php
+++ history.php
@@ -112,7 +112,7 @@
 				$num = $row["Hours"];
 				$vacation = $row["Vacation"];
 				echo "<tr><td><input type='hidden' name=$day-date value='$date'>$date</td>";
-				echo "<td><select name=$day-hours class='hidden' hidden>";
+				echo "<td><select name=$day-hours class='hidden' id='numhours' hidden>";
 				echo "<option value='0'></option>";
 				for ($i=1;$i<=24;$i++) {
 					if ($i == $num) {
@@ -146,7 +146,7 @@
 				$num = $row["Hours"];
 				$vacation = $row["Vacation"];
 				echo "<tr><td><input type='hidden' name=$day-date value='$date'>$date</td>";
-				echo "<td><select name=$day-hours class='hidden' hidden>";
+				echo "<td><select name=$day-hours class='hidden' id='numhours' hidden>";
 				echo "<option value='0'></option>";
 				for ($i=1;$i<=24;$i++) {
 					if ($i == $num) {
