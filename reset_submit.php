99aee09993cd9f4ccb0fe0760c2ebf1d8ab6c091
KranzA <107824196+KranzA@users.noreply.github.com>
2024-08-02 11:27:10 -0600

    Update reset_submit.php

--- reset_submit.php
+++ reset_submit.php
@@ -9,11 +9,11 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($_POST['pword'] == '' || ($_POST['repword'] == '' ) {
 	// Could not get the data that should have been sent.
 	exit('Please fill both fields!');
-} elseif ($_POST['pword'] != $_POST['repword']) {
+} /*elseif ($_POST['pword'] != $_POST['repword']) {
 	exit('Passwords do not match');
 } else {
 	echo "hi";
-        /*$stmt = $conn->prepare('SELECT id FROM accounts WHERE code = ?');
+        $stmt = $conn->prepare('SELECT id FROM accounts WHERE code = ?');
         echo md5($_POST['URLid']);
 	$stmt->bind_param('s', md5($_POST['URLid']));
 	$stmt->execute();
@@ -33,7 +33,6 @@ if ($_POST['pword'] == '' || ($_POST['repword'] == '' ) {
 			echo "five";
     		    header('Location: enter_hours.php');
     	    }
-	}*/
-}
-echo $_POST['URLid'];
+	}
+}*/
 ?>
