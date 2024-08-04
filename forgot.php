6a889341e93a47b25998bc88a75505858e889468
KranzGig <automail@kranztc.com>
2024-08-04 16:49:09 -0600

    Update forgot.php

--- forgot.php
+++ forgot.php
@@ -6,6 +6,7 @@
   <link rel="stylesheet" type="text/css" href="style.css"?v=1.2>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
 </head>
 <body onload="checkIncorrect()">
