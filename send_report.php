b61dbd8ed12a5c800bfbcdb252a534c1a124e3f9
KranzA <107824196+KranzA@users.noreply.github.com>
2024-08-07 15:35:43 -0600

    Update send_report.php

--- send_report.php
+++ send_report.php
@@ -4,19 +4,33 @@
 //echo $_POST['firstname'];
 $curdate = strtotime($_POST['firstdate']);
 $mins = 24 * 60 * 60;
+$myfile = fopen("result.csv", "w");
+fwrite($myfile, "Date, Hours, Vacation, EmpID\n");
 for ($x = date("w", $curdate); $x >= 0; $x--) {
 	$time = $curdate - $x * $mins;
 	$timestamp = date("m/d",$time);
-	//$sql = "SELECT Hours,Vacation FROM Hours WHERE Date=' $timestamp' AND EmpID=$id";
-	echo $timestamp;
-  echo "<br/>";
+	$sql = "SELECT Hours.Date, Hours.Hours, Hours.Vacation, accounts.name FROM Hours INNER JOIN accounts ON Hours.EmpID=accounts.id WHERE Date=' $timestamp' ORDER BY EmpID, Date";
+	$result = $conn->query($sql);
+	 while($row = $result->fetch_assoc()) {
+      //echo $row['Date'];
+      fwrite($myfile, $row["Date"] . ", ");
+      fwrite($myfile, $row["Hours"] . ", ");
+      fwrite($myfile, $row["Vacation"] . ", ");
+      fwrite($myfile, $row["name"] . ", \n");
+    }
       }
       for ($x = 1; $x < 7-date("w",$curdate); $x++) {
 	$time = $curdate + $x * $mins;
 	$timestamp = date("m/d",$time);
-//	$sql = "SELECT Hours,Vacation FROM Hours WHERE Date=' $timestamp' AND EmpID=$id";
-	echo $timestamp;
-	echo "<br/>";
+	$sql = "SELECT Hours.Date, Hours.Hours, Hours.Vacation, accounts.name FROM Hours INNER JOIN accounts ON Hours.EmpID=accounts.id WHERE Date=' $timestamp' ORDER BY EmpID, Date";    	
+	$result = $conn->query($sql);
+	 while($row = $result->fetch_assoc()) {
+      //echo $row['Date'];
+      fwrite($myfile, $row["Date"] . ", ");
+      fwrite($myfile, $row["Hours"] . ", ");
+      fwrite($myfile, $row["Vacation"] . ", ");
+      fwrite($myfile, $row["name"] . ", \n");
+    }
 	}
       
 ?>
