cfdc343969cdab4554fcc835bf2c06165afef01e
KranzA <107824196+KranzA@users.noreply.github.com>
2024-08-07 15:44:19 -0600

    Update send_report.php

--- send_report.php
+++ send_report.php
@@ -11,7 +11,7 @@ fwrite($myfile, "Date, Hours, Vacation, EmpID\n");
 for ($x = date("w", $curdate); $x >= 0; $x--) {
 	$time = $curdate - $x * $mins;
 	$timestamp = date("m/d",$time);
-	$sql = "SELECT Hours.Date, Hours.Hours, Hours.Vacation, accounts.name FROM Hours INNER JOIN accounts ON Hours.EmpID=accounts.id WHERE Date=' $timestamp' ORDER BY EmpID, Date";
+	$sql = "SELECT Hours.Date, Hours.Hours, Hours.Vacation, accounts.name FROM Hours INNER JOIN accounts ON Hours.EmpID=accounts.id ORDER BY EmpID, Date WHERE Date=' $timestamp'";
 	echo $sql;
 	$result = $conn->query($sql);
 	/* while($row = $result->fetch_assoc()) {
