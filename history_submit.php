6658ebdd28bcb4a1f24473b61d27f72e5ea127f8
KranzGig <automail@kranztc.com>
2024-08-06 20:16:03 -0600

    Update history_submit.php

--- history_submit.php
+++ history_submit.php
@@ -1,44 +1,44 @@
 <?php
-	session_start();
-	$servername = "127.0.0.1:3306";
-	$username = "u751975974_kranz";
-	$password = "Dradbgon12";
-	$dbname = "u751975974_TestDB";
+  session_start();
+  $servername = "127.0.0.1:3306";
+  $username = "u751975974_kranz";
+  $password = "Dradbgon12";
+  $dbname = "u751975974_TestDB";
 
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
 
-	// Check connection
-	if ($conn->connect_error) {
- 		die("Connection failed: " . $conn->connect_error);
-	}
-	$days = array("Sun","Mon","Tue","Wed","Thu","Fri","Sat");
-	foreach ($days as $day) {
-		$date = $_POST[$day . '-date'];
-		//echo $date;
-		$date = '"'.substr($date, strpos($date, ' ')).'"';
-		$id = $_POST['id'];
-		$hours = $_POST[$day . '-hours'];
-		$vacation = 0;
-		if (isset($_POST[$day . '-vacation'])) {
-			$vacation = 1;
-		}
-		//$vacation = $_POST[$day . 'pvacation'];
-		$sql = "SELECT * FROM Hours WHERE Date=$date AND EmpID=$id;";
-		//echo $sql;
-		$result = $conn->query($sql);
-		if ($result->num_rows > 0) {
-			$sql = "UPDATE Hours SET Hours=$hours, Vacation=$vacation WHERE Date=$date AND EmpID=$id";
-			//echo $sql;
-			$conn->query($sql);
-		}
-	}
-	$date = $_POST['Sun-date'];
-	$sql = "SELECT name FROM accounts WHERE id=$id";
-	$result = $conn->query($sql);
-	$row = $result->fetch_assoc();
-	$name = $row['name'];
-	//echo "done";
-	header("Location: history.php?name=$name&date=$date");
-		exit;
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  $days = array("Sun","Mon","Tue","Wed","Thu","Fri","Sat");
+  foreach ($days as $day) {
+    $date = $_POST[$day . '-date'];
+    //echo $date;
+    $date = '"'.substr($date, strpos($date, ' ')).'"';
+    $id = $_POST['id'];
+    $hours = $_POST[$day . '-hours'];
+    $vacation = 0;
+    if (isset($_POST[$day . '-vacation'])) {
+      $vacation = 1;
+    }
+    //$vacation = $_POST[$day . 'pvacation'];
+    $sql = "SELECT * FROM Hours WHERE Date=$date AND EmpID=$id;";
+    //echo $sql;
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+      $sql = "UPDATE Hours SET Hours=$hours, Vacation=$vacation WHERE Date=$date AND EmpID=$id";
+      //echo $sql;
+      $conn->query($sql);
+    }
+  }
+  $date = $_POST['Sun-date'];
+  $sql = "SELECT name FROM accounts WHERE id=$id";
+  $result = $conn->query($sql);
+  $row = $result->fetch_assoc();
+  $name = $row['name'];
+  //echo "done";
+  header("Location: history.php?name=$name&date=$date");
+  exit;
 ?>
