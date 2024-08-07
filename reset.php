a2ce7d8d3e675ed4303ca7ef1cf4c36e49c3ccad
KranzGig <automail@kranztc.com>
2024-08-06 20:02:45 -0600

    Update reset.php

--- reset.php
+++ reset.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <title>Time Tracker Login</title>
+  <meta charset="utf-8">
+  <title>Time Tracker Login</title>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,43 +11,42 @@
   <link rel="icon" href="http://documenthours.com/favicon.png">
 </head>
 <body onload="checkIncorrect()">
-<form action="reset_submit.php" method="post">
-    <?php
-// Initialize URL to the variable
-$url = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+  <form action="reset_submit.php" method="post">
+  <?php
+    // Initialize URL to the variable
+    $url = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
      
-// Use parse_url() function to parse the URL 
-// and return an associative array which
-// contains its various components
-$url_components = parse_url($url);
+    // Use parse_url() function to parse the URL 
+    // and return an associative array which
+    // contains its various components
+    $url_components = parse_url($url);
  
-// Use parse_str() function to parse the
-// string passed via URL
-parse_str($url_components['query'], $params);
-echo "<input type='hidden' name='URLid' value='".$params['id']."'/>";
-//echo $params['id'];
+    // Use parse_str() function to parse the
+    // string passed via URL
+    parse_str($url_components['query'], $params);
+    echo "<input type='hidden' name='URLid' value='".$params['id']."'/>";
+    //echo $params['id'];
  
-?> 
-<div class='reset'>
+    
+  ?> 
+  <div class='reset'>
     New Password:<br />
-      <input type="password" id="pword" name="pword" placeholder="Password"><br /><br />
-      Reenter Password:<br />
-      <input type="password" id="repword" name="repword" placeholder="Password"><br /><br />
-      <p id='incorrect' style="display:none;">Please enter a matching password in both fields</p>
-      <input type="submit" value="Enter">
-</div>
-</form>
+    <input type="password" id="pword" name="pword" placeholder="Password"><br /><br />
+    Reenter Password:<br />
+    <input type="password" id="repword" name="repword" placeholder="Password"><br /><br />
+    <p id='incorrect' style="display:none;">Please enter a matching password in both fields</p>
+    <input type="submit" value="Enter">
+  </div>
+  </form>
 
-<script type="text/javascript">
-	function checkIncorrect() {
-        const queryString = window.location.search;
-        const urlParams = new URLSearchParams(queryString);
-        if (urlParams.get("error") == "1") {
-            document.getElementById("incorrect").style.removeProperty('display');
-        }
+  <script type="text/javascript">
+    function checkIncorrect() {
+      const queryString = window.location.search;
+      const urlParams = new URLSearchParams(queryString);
+      if (urlParams.get("error") == "1") {
+        document.getElementById("incorrect").style.removeProperty('display');
+      }
     }
-    
-</script>
-    
+  </script>  
 </body>
 </html>
