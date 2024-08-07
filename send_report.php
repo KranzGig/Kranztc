efc7b80593b8c19f1e263661eee2a5c06f0dd9de
KranzA <107824196+KranzA@users.noreply.github.com>
2024-08-07 10:56:11 -0600

    Update send_report.php

--- send_report.php
+++ send_report.php
@@ -1,5 +1,22 @@
 <?php
-echo $_POST['firstdate'];
-echo "<br/>";
-echo $_POST['firstname'];
+//echo $_POST['firstdate'];
+//echo "<br/>";
+//echo $_POST['firstname'];
+$curdate = $_POST['firstdate'];
+$mins = 24 * 60 * 60;
+for ($x = date("w", $curdate); $x >= 0; $x--) {
+	$time = $curdate - $x * $mins;
+	$timestamp = date("m/d",$time);
+	//$sql = "SELECT Hours,Vacation FROM Hours WHERE Date=' $timestamp' AND EmpID=$id";
+	echo $timestamp;
+  echo "<br/>";
+      }
+      for ($x = 1; $x < 7-date("w",$curdate); $x++) {
+	$time = $curdate + $x * $mins;
+	$timestamp = date("m/d",$time);
+//	$sql = "SELECT Hours,Vacation FROM Hours WHERE Date=' $timestamp' AND EmpID=$id";
+	echo $timestamp;
+	echo "<br/>";
+	}
+      
 ?>
