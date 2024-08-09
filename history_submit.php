32ddeabb9011613fe13e4f9e7f9f609891c10953
KranzA <107824196+KranzA@users.noreply.github.com>
2024-08-09 14:28:41 -0600

    Update history_submit.php

--- history_submit.php
+++ history_submit.php
@@ -15,8 +15,9 @@
   $days = array("Sun","Mon","Tue","Wed","Thu","Fri","Sat");
   foreach ($days as $day) {
     $date = $_POST[$day . '-date'];
+    $time = strtotime(substr($date, strpos($date, ' ')));
     //echo $date;
-    $date = '"'.substr($date, strpos($date, ' ')).'"';
+    $date = '"'.date($time,"m/d/Y").'"';
     $id = $_POST['id'];
     $hours = $_POST[$day . '-hours'];
     $vacation = 0;
