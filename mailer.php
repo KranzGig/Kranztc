cf1f5184f2148b4d8b7c2317cbec45d6bbbd1c27
KranzA <107824196+KranzA@users.noreply.github.com>
2024-08-09 11:15:08 -0600

    Update mailer.php

--- mailer.php
+++ mailer.php
@@ -54,7 +54,7 @@
     while($row = $result->fetch_assoc()) {
       $mail->addAddress($row['email'], $row['name']);
       $mail->Subject = "Caretaker Hours Logged for Week of ".$firstdate;
-      $mail->Body = "Attached is the caretaker time-tracking report for the week of ".$firstdate.".\nIf you have any questions or concerns, please contact Cathy Limbach.\nThanks and Best Regards,\nDocument Hours Time Tracking Team";      $mail->addAttachment('result.csv');
+      $mail->Body = "Hi ".$row['name'].",\nAttached is the caretaker time-tracking report for the week of ".$firstdate.".\nIf you have any questions or concerns, please contact Cathy Limbach.\nThanks and Best Regards,\nDocument Hours Time Tracking Team";      $mail->addAttachment('result.csv');
       if (!$mail->send()) {
         echo 'Mailer Error: ' . $mail->ErrorInfo;
       } else {
