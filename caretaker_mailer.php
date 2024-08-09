ffbcad80a59f30a51a81b157c5bb472693f53cb1
KranzA <107824196+KranzA@users.noreply.github.com>
2024-08-09 11:26:35 -0600

    Update caretaker_mailer.php

--- caretaker_mailer.php
+++ caretaker_mailer.php
@@ -23,7 +23,7 @@
       echo $row['email'];
       $mail->addAddress($row['email'], $row['name']);
       $mail->Subject = "REMINDER:  Log Care-taking Hours";
-      $mail->Body = "Hi ".$row['name'].",\This is a quick reminder to log all of your care-taking hours for Martha for this week before the end of the day on Saturday.\n 
+      $mail->Body = "Hi ".$row['name'].",\This is a quick reminder to log all of your care-taking hours for Martha Carter for this week before the end of the day on Saturday.\n 
                     To log your hours, go to:  DocumentHours.com\n
                     You will be able to enter and change your hours for this week up through today.  Hours for future days this week can be entered on or after those days.\n
                     If you have any questions or problems, please contact Cathy Limbach.\n
