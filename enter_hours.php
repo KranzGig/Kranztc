2ecf15e04c1f787147bb6ac9815b842302acf9f7
KranzA <107824196+KranzA@users.noreply.github.com>
2024-08-09 14:23:52 -0600

    Update enter_hours.php

--- enter_hours.php
+++ enter_hours.php
@@ -60,7 +60,7 @@
 	  for ($x = date("w",$curtime); $x >= 0; $x--) {
 	    $time = $curtime - $x * $mins;
 	    $day = $name[date("w",$time)];
-	    $timestamp = date("m/d",$time);
+	    $timestamp = date("m/d/Y",$time);
 	    $date = $day." ".date("m/d",$time);
 	    $sql = "SELECT Hours,Vacation FROM Hours WHERE Date=' $timestamp' AND EmpID=$id";
 			
@@ -90,7 +90,7 @@
 	  for ($x = 1; $x < 7-date("w",$curtime); $x++) {
 	    $time = $curtime + $x * $mins;
 	    $day = $name[date("w",$time)];
-	    $timestamp = date("m/d",$time);
+	    $timestamp = date("m/d/Y",$time);
 	    $date = $day." ".date("m/d",$time);
 	    $sql = "SELECT Hours FROM Hours WHERE Date=' $timestamp' AND EmpID=$id";
 			
