80373d00c1cc75e23a7c4a19f5c788371e563ddf
KranzA <107824196+KranzA@users.noreply.github.com>
2024-08-09 11:33:13 -0600

    Update caretaker_mailer.php

--- caretaker_mailer.php
+++ caretaker_mailer.php
@@ -27,19 +27,19 @@
     $mail->setFrom('automail@kranztc.com', 'Kranz Tech Consulting');
     $mail->addReplyTo('automail@kranztc.com', 'Kranz Tech Consulting');
     $sql = "Select email,name FROM accounts WHERE admin=0";
-    echo $sql;
+    //echo $sql;
     $result = $conn->query($sql);
-    echo "one";
+    //echo "one";
     while($row = $result->fetch_assoc()) {
-      echo $row['email'];
+      //echo $row['email'];
       $mail->addAddress($row['email'], $row['name']);
       $mail->Subject = "REMINDER:  Log Care-taking Hours";
       $mail->Body = "Hi ".$row['name'].",\nThis is a quick reminder to log all of your care-taking hours for Martha Carter for this week before the end of the day on Saturday.\nTo log your hours, go to:  DocumentHours.com\nYou will be able to enter and change your hours for this week up through today.  Hours for future days this week can be entered on or after those days.\nIf you have any questions or problems, please contact Cathy Limbach at 303-378-5589.\nThanks and Best Regards,\n- Document Hours Time Tracking Team"; 
-      echo "two";
+      //echo "two";
       if (!$mail->send()) {
         echo 'Mailer Error: ' . $mail->ErrorInfo;
       } else {
-        echo "Email sent";
+        //echo "Email sent";
           }
     }
   ?>
