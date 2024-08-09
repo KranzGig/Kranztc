41d0827b5713f1e36d7ad36d2cf595ffa6bd48b2
KranzA <107824196+KranzA@users.noreply.github.com>
2024-08-09 14:34:12 -0600

    Update submission.php

--- submission.php
+++ submission.php
@@ -17,6 +17,7 @@
     $date = $_POST[$day . 'date'];
     echo $date;
     $time = strtotime(substr($date, strpos($date, ' ')));
+    echo $time;
     //echo $date;
     $date = '"'.date($time,"m/d/Y").'"';
     $id = $_SESSION['id'];
