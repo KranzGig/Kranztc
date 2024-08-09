58ec98a3ac40bfb53116625a68c73eb7b04c1871
KranzA <107824196+KranzA@users.noreply.github.com>
2024-08-09 11:28:29 -0600

    Update caretaker_mailer.php

--- caretaker_mailer.php
+++ caretaker_mailer.php
@@ -2,7 +2,18 @@
 <html>
 <body>    
   <?php
-   
+   $servername = "127.0.0.1:3306";
+    $username = "u751975974_kranz";
+    $password = "Dradbgon12";
+    $dbname = "u751975974_TestDB";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
     require 'vendor/autoload.php';
     use PHPMailer\PHPMailer\PHPMailer;
     $mail = new PHPMailer;
