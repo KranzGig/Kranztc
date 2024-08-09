99afb97f3cd155aad3bf81eecef9fd025434843f
KranzA <107824196+KranzA@users.noreply.github.com>
2024-08-09 14:32:21 -0600

    Update submission.php

--- submission.php
+++ submission.php
@@ -15,6 +15,7 @@
   $days = array("Sun","Mon","Tue","Wed","Thu","Fri","Sat");
   foreach ($days as $day) {
     $date = $_POST[$day . '-date'];
+    echo $date;
     $time = strtotime(substr($date, strpos($date, ' ')));
     //echo $date;
     $date = '"'.date($time,"m/d/Y").'"';
