b96f8c2966bdcb1667033bff91f4fc7418e7f9b2
KranzGig <automail@kranztc.com>
2024-09-05 13:06:25 -0600

    Update enter_hours.php

--- enter_hours.php
+++ enter_hours.php
@@ -127,6 +127,7 @@
   </form>
   <a href="logout.php"><button id='enter'>Logout</button></a>
 </div>
+<br /><br /><br />
 <p id="small"><a href="terms.html">Terms</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="privacy.html">Privacy Policy</a></p>
 </body>
 </html>
