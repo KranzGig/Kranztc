664d7794f4bd056b7c632f8d8ffda588b7ee8b7e
KranzGig <automail@kranztc.com>
2024-09-05 12:58:11 -0600

    Update index.php

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
     </div>
   </form>
   <p></p>
-  <p id="small"><a href="terms.html">Terms </a><a href="privacy.html">Privacy Policy</a></p>
+  <p id="small"><pre><a href="terms.html">Terms     </a><a href="privacy.html">Privacy Policy</a></pre></p>
   <script type="text/javascript">
     function checkIncorrect() {
       const queryString = window.location.search;
